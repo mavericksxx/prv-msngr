7dcc8abf7437dcdc4892dba6833b89c4315d8b3a
mavericksxx <130738587+mavericksxx@users.noreply.github.com>
2024-12-22 15:57:17 +0400

    added check auth route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -68,7 +69,7 @@ export const login = async (req, res) => {
             email: user.email,
             profilePic: user.profilePic,
         });
-        
+
     } catch (error) {
         console.log("Error in login controller:", error.message);
         res.status(500).json({ message: "Internal server error" });
@@ -82,5 +83,34 @@ export const logout = (req, res) => {
     } catch (error) {
         console.log("Error in logout controller:", error.message);
         res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+        
+        if (!profilePic) {
+            return res.status(400).json({ message: "Please upload an image" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true });
+
+        res.status(200).json(updatedUser);
+
+    } catch (error) {
+        console.log("Error in updateProfile controller:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const checkAuth = async (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller:", error.message);
+        res.status(500).json({ message: "Internal server error}" });
     }
 };
