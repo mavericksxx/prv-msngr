9ad79791552b7a7d4cdef5328ef83328d1c2061c
mavericksxx <130738587+mavericksxx@users.noreply.github.com>
2024-12-22 17:50:43 +0400

    added msg routes, schema and functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js"; 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"; 
 
 dotenv.config();
 const app = express();
@@ -14,6 +14,7 @@ app.use (express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use ("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:"+ PORT);
