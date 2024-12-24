cc4155763c71fada0354f790350ee9779e87f0b2
mavericksxx <130738587+mavericksxx@users.noreply.github.com>
2024-12-24 21:22:00 +0400

    pointless change dont bother seriously

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -83,7 +83,7 @@ const SignUpPage = () => {
                 <input
                   type="email"
                   className={`input input-bordered w-full pl-10`}
-                  placeholder="ds2sux@ds1supremacy.com"
+                  placeholder="example@email.com"
                   value={formData.email}
                   onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                 />
