4492479fd39de1dba92cf29680943e2a9e5dbc69
mavericksxx <130738587+mavericksxx@users.noreply.github.com>
2024-12-25 09:56:33 +0400

    added profile page functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -35,27 +35,41 @@ export const useAuthStore = create((set) => ({
         }
       },
 
-      logout: async () => {
-        try {
-          await axiosInstance.post("/auth/logout");
-          set({ authUser: null });
-          toast.success("Logged out successfully");
-        } catch (error) {
-          toast.error(error.response.data.message);
-        }
-      },
+    logout: async () => {
+      try {
+        await axiosInstance.post("/auth/logout");
+        set({ authUser: null });
+        toast.success("Logged out successfully");
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    },
 
-      login: async (data) => {
-        set({ isLoggingIn: true });
-        try {
-          const res = await axiosInstance.post("/auth/login", data);
-          set({ authUser: res.data });
-          toast.success("Logged in successfully");
-        } catch (error) {
-          toast.error(error.response.data.message);
-        } finally {
-          set({ isLoggingIn: false });
-        }
-      },
+    login: async (data) => {
+      set({ isLoggingIn: true });
+      try {
+        const res = await axiosInstance.post("/auth/login", data);
+        set({ authUser: res.data });
+        toast.success("Logged in successfully");
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isLoggingIn: false });
+      }
+    },
+
+    updateProfile: async (data) => {
+      set({ isUpdatingProfile: true });
+      try {
+        const res = await axiosInstance.put("/auth/update-profile", data);
+        set({ authUser: res.data });
+        toast.success("Profile updated successfully");
+      } catch (error) {
+        console.log("error in update profile:", error);
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isUpdatingProfile: false });
+      }
+    },
 
 }));
