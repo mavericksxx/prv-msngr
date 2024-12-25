c169c9341330399a4956454c8d63ff7cbec08e6c
mavericksxx <130738587+mavericksxx@users.noreply.github.com>
2024-12-25 09:53:44 +0400

    increased pfp upload size limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,9 @@ const app = express();
 
 const PORT = process.env.PORT
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use (express.json());
 app.use(cookieParser());
 app.use(
